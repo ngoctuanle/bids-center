1ff8bd6d6483eb391e04bce45f0bf0fd2e41a14c
cuongnv <vancuong140495@gmail.com>
2022-10-12 08:06:28 +0700

    Update logic for login form

--- src/layouts/MainLayout/LoginModal.tsx
+++ src/layouts/MainLayout/LoginModal.tsx
@@ -7,45 +7,52 @@ interface LoginModalProps {
 }
 
 export function LoginModal({ open, setOpen }: LoginModalProps) {
+  const [form] = Form.useForm();
+
+  const onClose = () => {
+    setOpen(false);
+    form.resetFields();
+  };
+
   const onFinish = (value: unknown) => {
     console.log(value);
     setOpen(false);
+    form.resetFields();
   };
 
   return (
-    <Modal open={open} title="Login" footer={null}>
-      {open && (
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          autoComplete="off"
+    <Modal open={open} title="Login" footer={null} onCancel={onClose}>
+      <Form
+        form={form}
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
         >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
-          >
-            <Input />
-          </Form.Item>
+          <Input />
+        </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password />
-          </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      )}
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </Modal>
   );
 }
